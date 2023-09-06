ba20c8c2c6024251eb7a09ce301f87a41fb060ee
FrancescoPepi <francescopepi16@gmail.com>
2023-09-06 17:54:06 +0200

    add bonus

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ createApp({
 
   methods: {
     generaMail() {
+      this.mail = [];
       for (let i = 0; i < 10; i++) {
         axios
           .get("https://flynn.boolean.careers/exercises/api/random/mail")
